65cf134fae247f605b889eb20579904b02915add
AshleighWong <ashleightwong@gmail.com>
2024-04-07 23:03:08 -0400

    updated homepage design css

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -3,14 +3,15 @@ src/components/AccountBalance.js
 
 The AccountBalance component displays account balance. It is included in other page views.
 ==================================================*/
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class AccountBalance extends Component {
   // Display account balance
   render() {
     return (
-      <div>
-        Balance: ${(Math.round(this.props.accountBalance* 100) / 100).toFixed(2)}
+      <div className="balance">
+        Account Balance: $
+        {(Math.round(this.props.accountBalance * 100) / 100).toFixed(2)}
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,27 +3,42 @@ src/components/Home.js
 
 The Home component is used to demonstrate the use of Link.
 ==================================================*/
-import React, {Component} from 'react';
-import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import AccountBalance from "./AccountBalance";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <img src="https://picsum.photos/200/200" alt="bank"/>
-
-        <h1>Bank of React</h1>
-
-        <Link to="/userProfile">User Profile</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="/credits">Credits (to be implemented in the Assignment)</Link>
-        <br/>
-        <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
-        <br/><br/>
-        <AccountBalance accountBalance={this.props.accountBalance}/>
+        <div className="header">
+          <h1>Welcome to The Bank of React</h1>
+        </div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/First_Bank_of_the_United_States%2C_Philadelphia%2C_Pennsylvania_LCCN2011633532_%28edited%29.jpg/800px-First_Bank_of_the_United_States%2C_Philadelphia%2C_Pennsylvania_LCCN2011633532_%28edited%29.jpg"
+          alt="bank"
+          className="bank"
+        />
+        <div className="link-group">
+          <Link to="/userProfile" className="custom-link">
+            User Profile
+          </Link>
+          <br />
+          <Link to="/login" className="custom-link">
+            Login
+          </Link>
+          <br />
+          <Link to="/credits" className="custom-link">
+            Credits
+          </Link>
+          <br />
+          <Link to="/debits" className="custom-link">
+            Debits
+          </Link>
+        </div>
+        <br />
+        <br />
+        <AccountBalance accountBalance={this.props.accountBalance} />
       </div>
     );
   }
